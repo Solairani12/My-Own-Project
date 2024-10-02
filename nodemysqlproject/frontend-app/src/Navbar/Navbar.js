d60a04034a563497fc6cb99ecbba6adaee6e348d
Solairani <132639210+Solairani12@users.noreply.github.com>
2024-10-02 04:00:57 -0400

    Navbar.js

--- nodemysqlproject/frontend-app/src/Navbar/Navbar.js
+++ nodemysqlproject/frontend-app/src/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';  // Add this CSS file for styling
+import './Navbar.css';  
 
 function Navbar() {
   return (
